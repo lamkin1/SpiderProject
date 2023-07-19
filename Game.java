27814f86d6d24d2f3e8cf1b189faa874fb287bbc
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-19 16:24:38 -0700

    updated

--- Game.java
+++ Game.java
@@ -7,56 +7,28 @@ public class Game extends JFrame{
     public Game(){
         super("Spider World");
         Border blackline = BorderFactory.createLineBorder(Color.black);
-        // main panel
-        JPanel main = new JPanel(new BorderLayout());
 
-        // west panel
+        JPanel mainPanel = new JPanel(new BorderLayout());
         setLayout(new BorderLayout());
-        JPanel gridPanel = new JPanel(); // change to WorldPanel
-        // WorldPanel worldPanel = new WorldPanel();
 
-        // Buttons (go into WorldPanel)
-        JButton step = new JButton("Step");
-        JButton turn = new JButton("Turn");
-        JButton red = new JButton("Red");
-        JButton blue = new JButton("Blue");
-        JButton green = new JButton("Green");
-        JButton black = new JButton("Black");
-
-
-        red.setBackground(Color.RED);
-        blue.setBackground(Color.BLUE);
-        green.setBackground(Color.GREEN);
-        black.setBackground(Color.BLACK);
-
-        gridPanel.add(step);
-        gridPanel.add(turn);
-        gridPanel.add(red);
-        gridPanel.add(blue);
-        gridPanel.add(green);
-        gridPanel.add(black);
-
+        WorldPanel worldPanel = new WorldPanel();
         WorkAreaPanel workAreaPanel = new WorkAreaPanel();
 
         JPanel levels = new JPanel();
         JButton level1 = new JButton("1");
         levels.add(level1);
 
-        gridPanel.setBorder(blackline);
         workAreaPanel.setBorder(blackline);
         levels.setBorder(blackline);
 
-        main.add(gridPanel,BorderLayout.WEST);
-        main.add(levels, BorderLayout.NORTH);
+        mainPanel.add(levels, BorderLayout.NORTH);
+        mainPanel.add(worldPanel,BorderLayout.WEST);
+        mainPanel.add(workAreaPanel,BorderLayout.EAST);
 
-        //main.add(worldPanel,BorderLayout.WEST);
-        main.add(workAreaPanel,BorderLayout.EAST);
-
-        // work area (center)
-        add(main);
+        add(mainPanel);
     }
 
-    public static void main(String[] args) {
+    public static void mainPanel(String[] args) {
         Game Game = new Game();
         Game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Game.setSize(1000,600);
