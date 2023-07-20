75d7852136912113d98e4bd128f0c60170ef669b
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-19 17:17:19 -0700

    updated

--- WorldPanel.java
+++ WorldPanel.java
@@ -1,1 +1,54 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.*;
+public class WorldPanel extends JPanel implements ActionListener{
+    private int x1, x2, y1, y2;
+    World world = new World();
+    //ConnectHelper connectHelper = new ConnectHelper();
+    public WorldPanel(){
+        this.setPreferredSize(new Dimension(500, 500));
+        this.setBackground(Color.WHITE);
 
+        JButton step = new JButton("Step");
+        JButton turn = new JButton("Turn");
+        JButton red = new JButton("Red");
+        JButton blue = new JButton("Blue");
+        JButton green = new JButton("Green");
+        JButton black = new JButton("Black");
+
+        red.setOpaque(true);
+        blue.setOpaque(true);
+        green.setOpaque(true);
+        black.setOpaque(true);
+
+        red.setBackground(Color.RED);
+        blue.setBackground(Color.BLUE);
+        green.setBackground(Color.GREEN);
+        black.setBackground(Color.BLACK);
+
+        step.addActionListener(this);
+        turn.addActionListener(this);
+        red.addActionListener(this);
+        blue.addActionListener(this);
+        green.addActionListener(this);
+        black.addActionListener(this);
+
+        this.add(step);
+        this.add(turn);
+        this.add(red);
+        this.add(blue);
+        this.add(green);
+        this.add(black);
+    }
+
+    public void paintComponent(Graphics g){
+        // loop through struct that holds cells, spider, diamonds
+        world.draw(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
