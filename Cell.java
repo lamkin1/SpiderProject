1d26c66ff2f46d3aa5bb1c621ea8bbdc523b94e7
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-21 15:58:10 -0700

    updated

--- Cell.java
+++ Cell.java
@@ -2,33 +2,42 @@ import java.awt.*;
 
 public class Cell {
     private int id;
+    private int x, y;
     Spider spider;
+    Diamond d;
     private Boolean hasDiamond, hasSpider, isEmpty;
     private Color color = Color.black; // default is black
 
-    public Cell(int id, Boolean hasDiamond, Boolean hasSpider, Boolean isEmpty){
+    public Cell(int x, int y, int id, Boolean hasDiamond, Boolean hasSpider, Boolean isEmpty, Color diamondColor){
+        this.x = x;
+        this.y = y;
         this.id = id;
         this.hasDiamond = hasDiamond;
         this.hasSpider = hasSpider;
         this.isEmpty = isEmpty;
         spider = new Spider(x, y, "up");
+        d = new Diamond(diamondColor, x, y);
+
     }
 
     // 25 by 25 for block
-    public void draw(Graphics g){
+    public void draw(int x, int y, Graphics g){
         g.setColor(this.color);
         g.drawRect(x, y, 25, 25);
+        //
 
         if (hasSpider){
-
             spider.draw(g);
         }
 
         if (hasDiamond){
-
+            d.draw(g);
         }
     }
 
+    public int getX() {return x;}
+
+    public int getY() {return y;}
 
     public int getId() {
         return id;
@@ -48,6 +57,14 @@ public class Cell {
 
     public Color getColor() {
         return color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public void setId(int id) {
