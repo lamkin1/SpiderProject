b76649485e31371bfbee19792f05a753e39785cf
lamkin1 <jamesclamkin@gmail.com>
2023-07-21 17:06:53 -0700

    added block and actionblock

---
+++ ActionBlock.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.awt.*;
+
+public class ActionBlock extends Block{
+    private int x_len = 10, y_len = 5;
+    private String name;
+    private Color color;
+
+    public ActionBlock(int x1, int y1, String action){
+        super.blocks = DataSource.getInstance().getBlockArrayInstance();
+        super.setX1(x1);
+        super.setX2(x1 + x_len);
+        super.setY1(y1);
+        super.setY2(y1 + y_len);
+        switch(action){
+            case "Step":    name = "Step";
+                            color = Color.GRAY;
+                            break;
+            case "Turn":    name = "Turn";
+                            color = Color.GRAY;
+                            break;
+            case "Paint":   name = "Paint";
+                            color = Color.ORANGE;
+                            break;
+        }
+    }
+
+    public void run(){;}
+
+    public void draw(Graphics g){
+        g.setColor(color);
+        g.fillRect(super.getX1(), super.getY1(), super.getX2(), super.getY2());
+    }
+
+    public void connect(){
+        int x_avg = super.getX1()+super.getX2()/2;
+        Block temp, temp2, transfer;
+        for(int i = 0; i < super.blocks.size(); i++){
+            temp = super.blocks.get(i);
+            do{
+                if((this.getY1() >= temp.getY2() - 2 && super.getY1() <= temp.getY2() + 2) && (x_avg >= temp.getX1() && x_avg <= temp.getX2())){
+                    this.setY1(temp.getY2());
+                    this.setX1(temp.getX1());
+                    this.setX2(temp.getX2());
+                    this.setY2(this.getY1()+y_len);
+                    transfer = temp.next;
+                    temp.next = this;
+                    this.next = transfer;
+                    temp2 = transfer;
+                    do{
+                        temp2.shift(y_len);
+                        temp2 = temp2.next;
+                    }while(temp2 != null);
+                }
+                temp = temp.next;
+            }while(temp != null);
+        }
+    }
+
+
+
+
+
+}
---
+++ Block.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
+//NEED TO IMPLEMENT STEP, TURN, PAINT, AND 1 LOOP BLOCK; LOOP BLOCK IS ONLY TIME TO USE DECORATOR
+public abstract class Block{
+    Block next = null;
+    private int x1, y1, x2, y2;
+    ArrayList<Block> blocks;
+    public abstract void draw(Graphics g);
+
+    public void moving(int x_diff, int y_diff){
+        x1 = x1 + x_diff;
+        x2 = x2 + x_diff;
+        y1 = y1 + y_diff;
+        y2 = y2 + y_diff;
+    }
+
+
+    public void shift(int y_diff){
+        moving(0, y_diff);
+    }
+
+
+    public abstract void run();
+
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+}
