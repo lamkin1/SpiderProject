9115bba0722d4e6a5c15a6ba124eab2f489696eb
lamkin1 <jamesclamkin@gmail.com>
2023-07-23 16:10:18 -0700

    changed drawRect to fillRect

--- Cell.java
+++ Cell.java
@@ -15,7 +15,7 @@ public class Cell {
     public void draw(Graphics g){
         // maybe better to draw in world and get info from data source
         g.setColor(this.color);
-        g.drawRect(x, y, 25, 25);
+        g.fillRect(x, y, 25, 25);
 
     }
 
