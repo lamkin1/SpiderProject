953aecb64bb9f487952a06d439e46cabbf4cf8db
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-24 21:05:27 -0700

    added trashcan

--- WorkAreaPanel.java
+++ WorkAreaPanel.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionListener, ActionListener {
     private int x1, x2, y1, y2;
     private BlockSpawner step;
-    private BlockSpawner paint;
+    private BlockSpawner paintRed;
+    private BlockSpawner paintBlue;
+    private BlockSpawner paintGreen;
     private BlockSpawner turn;
 
     int offsetX, offsetY;
@@ -19,6 +21,8 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
     //private Block chosenBlock;
     //ConnectHelper connectHelper = new ConnectHelper();
     public WorkAreaPanel() {
+        addMouseListener(this);
+        addMouseMotionListener(this);
         this.setPreferredSize(new Dimension(500, 500));
         setLayout(new BorderLayout());
 
@@ -27,14 +31,29 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
         this.setBackground(Color.WHITE);
         step = new BlockSpawner(450, 100, Color.ORANGE, "Step");
         turn = new BlockSpawner(450, 100, Color.ORANGE, "Turn");
-        paint = new BlockSpawner(450, 100, Color.ORANGE, "Paint");
+        paintRed = new BlockSpawner(450, 100, Color.RED, "Paint Red");
+        paintRed.setBackground(Color.RED);
+        paintRed.setOpaque(true);
+        paintRed.setBorderPainted(false);
+        paintBlue = new BlockSpawner(450, 100, Color.BLUE, "Paint Blue");
+        paintBlue.setBackground(Color.BLUE);
+        paintBlue.setOpaque(true);
+        paintBlue.setBorderPainted(false);
+        paintGreen = new BlockSpawner(450, 100, Color.GREEN, "Paint Green");
+        paintGreen.setBackground(Color.GREEN);
+        paintGreen.setOpaque(true);
+        paintGreen.setBorderPainted(false);
 
         buttonPanel.add(step);
         buttonPanel.add(turn);
-        buttonPanel.add(paint);
+        buttonPanel.add(paintRed);
+        buttonPanel.add(paintBlue);
+        buttonPanel.add(paintGreen);
 
         step.addActionListener(this);
-        paint.addActionListener(this);
+        paintRed.addActionListener(this);
+        paintBlue.addActionListener(this);
+        paintGreen.addActionListener(this);
         turn.addActionListener(this);
 
         buttonPanel.setLayout(new GridLayout(6, 1));
@@ -45,6 +64,7 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
 
     public void paintComponent(Graphics g) {
         for(Block block: blocks){
+            System.out.println("drawing");
             block.draw(g);
         }
     }
@@ -56,12 +76,9 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
 
     @Override
     public void mousePressed(MouseEvent e) {
-        System.out.println("here");
         //x_len = 30, y_len = 15;
         x1 = e.getX();
         y1 = e.getY();
-
-        System.out.println(e.getSource().getClass());
 
         for(Block block: blocks){
             if (block instanceof ActionBlock && (block.getX1() <= x1 && x1 <= block.getX1() + 30) && (block.getY1() <= y1 && y1 <= block.getY1() + 15)) { // change to follow if inside block
@@ -72,7 +89,6 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
                 break;
             }
         }
-
     }
 
     @Override
@@ -80,11 +96,15 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
         x2 = e.getX();
         y2 = e.getY();
 
+        System.out.println("here in dragged");
+
         int x_diff = x2 - offsetX;
         int y_diff = y2 - offsetY;
 
         selectedActionBlock.setX1(x_diff);
         selectedActionBlock.setY1(y_diff);
+
+        //selectedActionBlock.moving(x_diff, y_diff);
 
         repaint();
     }
@@ -126,7 +146,21 @@ public class WorkAreaPanel extends JPanel implements MouseListener, MouseMotionL
                 repaint();
             }
 
-            if (((JButton) e.getSource()).getText().equals("Paint")) {
+            if (((JButton) e.getSource()).getText().equals("Paint Red")) {
+                System.out.println("making paint block");
+                ActionBlock ab = new ActionBlock(300, 150, "Paint");
+                blocks.add(ab);
+                repaint();
+            }
+
+            if (((JButton) e.getSource()).getText().equals("Paint Blue")) {
+                System.out.println("making paint block");
+                ActionBlock ab = new ActionBlock(300, 150, "Paint");
+                blocks.add(ab);
+                repaint();
+            }
+
+            if (((JButton) e.getSource()).getText().equals("Paint Green")) {
                 System.out.println("making paint block");
                 ActionBlock ab = new ActionBlock(300, 150, "Paint");
                 blocks.add(ab);
