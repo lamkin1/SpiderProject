b344db677cfa6718a0601c37ce72315a78a130fe
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-26 12:37:18 -0700

    fixed reset

--- Game.java
+++ Game.java
@@ -103,62 +103,77 @@ public class Game extends JFrame implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().getClass().getName().equals("javax.swing.JButton")) {
             if (((JButton) e.getSource()).getText().equals("1")) {
+                DataSource.getInstance().setLevel(1);
                 lh.load(1);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("2")) {
+                DataSource.getInstance().setLevel(2);
                 lh.load(2);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("3")) {
+                DataSource.getInstance().setLevel(3);
                 lh.load(3);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("4")) {
+                DataSource.getInstance().setLevel(4);
                 lh.load(4);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("5")) {
+                DataSource.getInstance().setLevel(5);
                 lh.load(5);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("6")) {
+                DataSource.getInstance().setLevel(6);
                 lh.load(6);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("7")) {
+                DataSource.getInstance().setLevel(7);
                 lh.load(7);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("8")) {
+                DataSource.getInstance().setLevel(8);
                 lh.load(8);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("9")) {
+                DataSource.getInstance().setLevel(9);
                 lh.load(9);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("10")) {
+                DataSource.getInstance().setLevel(10);
                 lh.load(10);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("11")) {
+                DataSource.getInstance().setLevel(11);
                 lh.load(11);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("12")) {
+                DataSource.getInstance().setLevel(12);
                 lh.load(12);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("13")) {
+                DataSource.getInstance().setLevel(12);
                 lh.load(13);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("14")) {
+                DataSource.getInstance().setLevel(14);
                 lh.load(14);
                 worldPanel.repaint();
             }
             if (((JButton) e.getSource()).getText().equals("15")) {
+                DataSource.getInstance().setLevel(15);
                 lh.load(15);
                 worldPanel.repaint();
             }
