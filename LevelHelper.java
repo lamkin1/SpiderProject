5847f33aca4ce71219486b1d983dd1c8ee8866fc
0felicia0 <86893879+0felicia0@users.noreply.github.com>
2023-07-26 16:46:36 -0700

    buttons, size, popups

--- LevelHelper.java
+++ LevelHelper.java
@@ -47,7 +47,6 @@ public class LevelHelper {
             System.err.println("Error reading file " + fileName + ": " + e.getMessage());
         }
         fillDataSource();
-
     }
 
     public int getLevel(){
---
+++ PopUp.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class PopUp extends JFrame{
+    private String instructions;
+    public PopUp(String instructions){
+        this.instructions = instructions;
+    }
+
+
+}
